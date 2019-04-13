31a8d34c4e77d313aa09041c6cab8c85614fa111
nadia12344 <npranabudi@gmail.com>
2019-04-13 17:37:28 +1000

    homepage

---
+++ codebrew/src/SearchDrug.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SearchDrug.css';
+
+class SearchDrug extends Component {
+  render() {
+    return (
+      <div>
+          <p>search drug</p>
+      </div>
+    );
+  }
+}
+
+export default SearchDrug;
