d5749495d6323c14b4b511c6a1aae5de8e9759fe
Arpit Bajaj <arpitbajaj98@gmail.com>
2019-04-13 16:56:28 +1000

    Initial stuff

---
+++ app.js
@@ -0,0 +1,22 @@
+var express                 = require("express"),
+    bodyParser              = require("body-parser"),
+    mongoose                = require("mongoose"),
+    methodOverride          = require("method-override");
+
+var app 					= express();
+
+var indexRoutes				= require("./routes/index");
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__+"/public"));
+app.use(methodOverride("_method"));
+
+
+app.use("/", indexRoutes);
+
+
+app.listen(3000, "0.0.0.0", function(){
+	console.log("Drug help server has started");
+});
